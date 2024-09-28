3de93c030b383ce58e12ee9343673ed6b9b67071
HPR98 <henry@hpr-lighting.com>
2024-09-28 15:46:23 +0200

    Added Add, Delete, and started to work on edit (work in progress)

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 
@@ -7,7 +7,7 @@ app = Flask("__name__")
 CORS(app)
 
 @app.route("/get_contacts", methods=["GET"])
-def add_contact():
+def get_contacts():
     
     conn = sqlite3.connect("database.db")
     cursor = conn.cursor()
@@ -25,12 +25,60 @@ def add_contact():
             "last_name": element[2],
             "email": element[3]
         }
-    
-    print(object)
+
     return object, 200
 
+@app.route("/add_contact", methods=["POST"])
+def add_contact():
+    first_name  = request.json.get("first_name")
+    last_name = request.json.get("last_name")
+    email = request.json.get("email")
+    
+    print(first_name, last_name, email)
+    
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    
+    table = "INSERT INTO contacts (first_name, last_name, email) VALUES (?,?,?)"
+    
+    cursor.execute(table, (first_name, last_name, email))
+    
+    conn.commit()
+    conn.close()
+    
+    return jsonify({"message": "Contact succesfully added to server"}), 200
 
+@app.route("/delete_contact", methods=["POST"])
+def delete_contact():
+    data = int(request.json.get("key"))
+    
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("DELETE FROM contacts WHERE id = ?", (data,))
+    
+    conn.commit()
+    conn.close()
+    
+    return jsonify({"message": "Contact successfully deleted!"}), 200
 
+@app.route("/edit_contact", methods=["POST"])
+def edit_contact():
+    key = request.json.get("key")
+    first_name = request.json.get("first_name")
+    last_name = request.json.get("last_name")
+    email = request.json.get("email")
+    
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+    
+    table = f"UPDATE contacts SET first_name = ?, last_name = ?, email = ? WHERE id = {key}"
+    
+    cursor.execute(table, (first_name, last_name, email))
+    conn.commit()
+    conn.close()
+    
+    return jsonify({"message": "Contact succesfully updated!"}), 200
 
 
 if __name__ == "__main__":
--- Frontend/contactapp/src/App.js
+++ Frontend/contactapp/src/App.js
@@ -9,16 +9,175 @@ const getConntacts = async () => {
       throw new Error ("Could not fetch data from endpoint")
     }
     const data  = await response.json()
-    console.log(data)
     return data
   } catch(error){
     console.error(error)
   }
 }
 
-function App() {
+const AddContact = ({ isVisible, setIsVisible }) => {
+const [firstName, setFirstName] = useState("")
+const [lastName, setLastName] = useState("")
+const [email, setEmail] = useState("")
 
+const handleSubmit = async(e) => {
+  e.preventDefault();
+
+  const contactData = {
+    first_name: firstName,
+    last_name: lastName, 
+    email: email,
+  }
+
+  try{
+    const response = await fetch("http://127.0.0.1:5000/add_contact", {
+      method: "POST",
+      headers: {"Content-Type": "application/json",},
+      body: JSON.stringify(contactData)
+    })
+    if(response.ok) {
+      const data = await response.json()
+      console.log("Contact succesfully send to server!", data)
+      window.location.reload()
+    }
+  }catch(error){
+    console.error("Error:", error)
+  }
+};
+
+
+  if(isVisible){
+    return(
+      <div className='AddContact'>
+        <button onClick={() => setIsVisible(false)}>X</button>
+        <h2>Add contact</h2>
+        <form className='addContactForm' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            placeholder='First Name'
+            value = {firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+          <input
+            type='text'
+            placeholder='Last Name'
+            value = {lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+          <input
+            type='text'
+            placeholder='E-Mail'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type='submit'>Submit</button>
+        </form>
+          
+      </div>
+    )
+  }
+  return null;
+}
+
+const deleteContact = async(key) => {
+  const data = {"key": key}
+
+  try{
+    const response = await fetch("http://127.0.0.1:5000/delete_contact",{
+      method: "POST",
+      headers: {"Content-Type": "application/json",},
+      body: JSON.stringify(data)
+    })
+    if(response.ok){
+      const message = await response.json()
+      console.log(message)
+      window.location.reload()
+    }
+  }catch(error){
+    console.error("Hi there was a problem:", error)
+  }
+}
+
+
+
+function App() {
   const [contacts, setContacts] = useState([])
+  const [isVisible, setIsVisible] = useState(false);
+  const [currentKey, setCurrentKey] = useState("")
+  const [currentContact, setCurrentContact] = useState(null)
+
+  const EditContact = () => {
+    const [formFirstName, setFormFirstName] = useState("")
+    const [formLastName, setLastName] = useState("")
+    const [formEmail, setEmail] = useState("")
+
+    setCurrentContact()
+
+
+    const submitChanges = async(e, key) => {
+      e.preventDefault()
+
+      const contactData = {
+        key: key,
+        first_name: formFirstName,
+        last_name: formLastName,
+        email: formEmail
+      }
+
+      try{
+        const response = await fetch("http://127.0.0.1:5000/edit_contact",{
+          method: "POST",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify(contactData),
+        })
+        if(response.ok){
+          const data = await response.json()
+          console.log("Data succesfully send to server", data)
+        }
+
+      }catch(error){
+        console.error("There was a problem: ", error)
+      }
+      window.location.reload()
+    
+    }
+  
+
+    if(true){
+      return(
+        <div>
+          <button>x</button>
+          <form className='editForm' onSubmit={(e) => submitChanges(e, currentKey)}>
+            <input
+              type='text'
+              placeholder='First name'
+              value= {formFirstName}
+              onChange={(e) => setFormFirstName(e.target.value)}
+              id='editFormFirstName'
+            />
+            <input
+              type="text"
+              placeholder='Last name'
+              value={formLastName}
+              onChange={(e) => setLastName(e.target.value)}
+              id="editFormLastName"
+            />
+            <input
+              type="text"
+              placeholder="email"
+              value={formEmail}
+              onChange={(e) => setEmail(e.target.value)}
+              id="editFormEmail"
+            />
+            <button type='submit'>Submit changes</button>
+          </form>
+        </div>
+      )
+    }
+  }
+  
 
   useEffect(() => {
     const fetchConntacts = async () => {
@@ -30,7 +189,14 @@ function App() {
 
   return (
     <div>
-      <h1>Test</h1>
+      <h1 id="title">Contacts</h1>
+      <div>
+        <button onClick={() => setIsVisible(true)}>Add Contact</button>
+      </div>
+      <div>
+        <AddContact isVisible={isVisible} setIsVisible={setIsVisible}/>
+        <EditContact currentContact = {currentContact} />
+      </div>
       <table>
         <thead>
           <tr>
@@ -43,10 +209,12 @@ function App() {
           {Object.keys(contacts).map(key => {
             const { first_name, last_name, email } = contacts[key];
             return (
-              <tr key={key}>
+              <tr key={key} id={key}>
                 <td>{first_name}</td>
                 <td>{last_name}</td>
                 <td>{email}</td>
+                <td><button onClick={() => deleteContact(key)}>Delete</button></td>
+                <td><button onClick={() => setCurrentKey(key)}>Edit</button></td>
               </tr>
             )
           })}
